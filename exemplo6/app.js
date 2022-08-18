a80a7e07baaf036ef41ea1d0c559a1b7f4d1c2a3
Guilherme Santana <g.santossantana22@gmail.com>
2022-08-18 11:46:55 -0300

    node view

--- exemplo6/app.js
+++ exemplo6/app.js
@@ -1,8 +1,10 @@
 import express from  "express"; 
 
 const app = express();
+app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{ 
-    res.send('Eu vou ganhar na mega');
+    /* res.send('teste qualquer') para ver se esta funcionando*/
+    res.render('paginas/index');
     
 });
 /* executando o servidor */
@@ -10,5 +12,10 @@ app.listen(8080, ()=> { console.log('Servidor Rodando...');});
 
 app.get('/sobre', (req, res) => {
     
-    res.send('Sobre');
+    res.render('paginas/sobre');
+});
+
+app.get('/contato', (req, res) => {
+    
+    res.render('paginas/contato');
 });
